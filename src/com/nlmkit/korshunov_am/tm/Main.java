e7f763e96150a2c81d81d88747e322f2d7e5bb4a
Андрей Коршунов <robokot0@gmail.com>
2020-07-10 18:40:17 +0500

    Реализована поддержка аргументов запуска терминального приложенния.

--- src/com/nlmkit/korshunov_am/tm/Main.java
+++ src/com/nlmkit/korshunov_am/tm/Main.java
@@ -1,8 +1,54 @@
 package com.nlmkit.korshunov_am.tm;
 
+import java.util.Arrays;
+
+/**
+ *  Тестовое приложение
+ */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD!");
+        run(args);
+        displayWelcome();
     }
+    /**
+     * Обработка параметров
+     * @param args список параметров командной строки
+     */
+    private static void run(final String[] args) {
+        if (args == null) return;
+        if (args.length <1) return;
+        final String param = args[0];
+        if ("version".equals(param)) displayVersion();
+        if ("about".equals(param)) displayAbout();
+        if ("help".equals(param)) displayHelp();
+        System.out.println("Unknown parameter values");
+        System.out.println(Arrays.toString(args));
+        displayHelp();
+    }
+
+    private static void displayWelcome(){
+        System.out.println("**** WELCOME TO TASK MANAGER ****");
+    }
+
+    private static void displayVersion() {
+        System.out.println("1.0.0");
+        System.exit(0);
+    }
+
+    private static void displayHelp() {
+        System.out.println("Possible parameter values:");
+        System.out.println("version - Display version");
+        System.out.println("about - Display developer info");
+        System.out.println("help - Display list of command");
+        System.exit( 0);
+
+    }
+
+    private static void displayAbout() {
+        System.out.println("Andrey Korshunov");
+        System.out.println("korshunov_am@nlmk.com");
+        System.exit( 0);
+    }
+
 }
